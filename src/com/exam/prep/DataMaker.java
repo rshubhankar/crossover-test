bbb4a91ef19cec2f1311a6e28bb6ef59ffe77601
Shubhankar Roy <rshubhankar@yahoo.co.in>
2015-12-25 10:28:02 +0530

    First Commit

---
+++ src/com/exam/prep/DataMaker.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.exam.prep;
+
+/**
+ * @author Shubhankar
+ *
+ */
+public class DataMaker {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
