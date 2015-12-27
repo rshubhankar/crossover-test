8b7ad28e5bc8d66f3402590ecf15a28c68efa027
Shubhankar Roy <rshubhankar@yahoo.co.in>
2015-12-27 23:07:31 +0530

    Changed the data to create a meaningful exam.

--- create_data.sql
+++ create_data.sql
@@ -8,17 +8,41 @@ INSERT INTO `candidate` VALUES (1,'admin','admin123');
 -- Dumping data for table `exam`
 --
 
-INSERT INTO `exam` VALUES (1,'Test Description',30,'MINUTES');
+INSERT INTO `exam` VALUES (1,'General Quiz with simple and easy questions. Time Limit for this test is 30 Minutes.',30,'MINUTES');
 
 --
 -- Dumping data for table `question`
 --
 
-INSERT INTO `question` VALUES (1,1,'Question 1'),(2,1,'Question 2'),(3,1,'Question 3'),(4,1,'Question 4'),(5,1,'Question 5');
+INSERT INTO `question` VALUES 
+(1,1,'What is the capital of India ?'),
+(2,1,'Which is the 4th planet in the solar system ?'),
+(3,1,'What is 7 multiplied to 8 ?'),
+(4,1,'In television serial Friends, Ross was given a title. Which of the following was it ? Was it:'),
+(5,1,'Who was called ''Bapu'' in India ?');
 
 --
 -- Dumping data for table `answer`
 --
 
-INSERT INTO `answer` VALUES (1,'Correct Answer 1',1,1),(2,'Answer 2',NULL,1),(3,'Answer 3',NULL,1),(4,'Answer 4',NULL,1),(5,'Answer 1',NULL,2),(6,'Correct Answer 2',1,2),(7,'Answer 3',NULL,2),(8,'Answer 4',NULL,2),(9,'Correct Answer 1',1,3),(10,'Answer 2',NULL,3),(11,'Answer 3',NULL,3),(12,'Answer 4',NULL,3),(13,'Answer 1',NULL,4),(14,'Answer 2',NULL,4),(15,'Correct Answer 3',1,4),(16,'Answer 4',NULL,4),(17,'Correct Answer 1',1,5),(18,'Answer 2',NULL,5),(19,'Answer 3',NULL,5),(20,'Answer 4',NULL,5);
-/*!40000 ALTER TABLE `answer` ENABLE KEYS */;
+INSERT INTO `answer` VALUES 
+(1,'Delhi',1,1),
+(2,'Mumbai',NULL,1),
+(3,'Chennai',NULL,1),
+(4,'Kolkata',NULL,1),
+(5,'Jupiter',NULL,2),
+(6,'Mars',1,2),
+(7,'Earth',NULL,2),
+(8,'Venus',NULL,2),
+(9,'48',NULL,3),
+(10,'40',NULL,3),
+(11,'72',NULL,3),
+(12,'56',1,3),
+(13,'The Enforcer',NULL,4),
+(14,'The Guardian',NULL,4),
+(15,'The Divorcer',1,4),
+(16,'The Machinist',NULL,4),
+(17,'Mohandas Karamchand Gandhi',1,5),
+(18,'Aamir Khan',NULL,5),
+(19,'Sachin Tendulkar',NULL,5),
+(20,'Johnny Lever',NULL,5);
